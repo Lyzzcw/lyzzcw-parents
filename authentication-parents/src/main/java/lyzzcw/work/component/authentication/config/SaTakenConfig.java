88cd60080f60622884d1aa51544bec4047f881f7
lzy <1204375052@qq.com>
2023-07-13 11:29:09 +0800

    swagger3 config

--- authentication-parents/src/main/java/lyzzcw/work/component/authentication/config/SaTakenConfig.java
+++ authentication-parents/src/main/java/lyzzcw/work/component/authentication/config/SaTakenConfig.java
@@ -29,7 +29,10 @@ public class SaTakenConfig implements WebMvcConfigurer {
         registry.addInterceptor(new SaInterceptor( handler -> {
             // 登录认证 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
             SaRouter.match("/**", "/user/doLogin", r -> StpUtil.checkLogin());
-
+//            SaRouter.match("/**")
+//                    .notMatch("/user/doLogin")
+//                    .notMatch("/swagger-ui/**")
+//                    .check(r -> StpUtil.checkLogin());
             // 角色认证 -- 拦截以 admin 开头的路由，必须具备 admin 角色或者 super-admin 角色才可以通过认证
             SaRouter.match("/admin/**", r -> StpUtil.checkRoleOr("admin", "super-admin"));
 
---
+++ swagger-parents/src/main/java/lyzzcw/work/component/swagger/config/SwaggerConfig.java
@@ -0,0 +1,79 @@
+package lyzzcw.work.component.swagger.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+@EnableOpenApi
+@Configuration
+public class SwaggerConfig {
+
+    //http://Ip:Port/swagger-ui/index.html
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .enable(true)
+//                .ignoredParameterTypes(HttpSession.class, HttpServletRequest.class, HttpServletResponse.class)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title( "模块--Swagger文档")
+                .description("接口说明文档")
+                .contact(new Contact("name", "url", "@163.com"))
+                .version("1.0-SNAPSHOT")
+                .build();
+    }
+
+    //  一、
+    //  mvc:
+    //    pathmatch:
+    //      matching-strategy: ant_path_matcher
+
+    //  二、解决 swagger3.0 与 srpring boot 2.6.x 冲突启动失败问题
+//    @Bean
+//    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+//        return new BeanPostProcessor() {
+//
+//            @Override
+//            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+//                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+//                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+//                }
+//                return bean;
+//            }
+//
+//            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+//                List<T> copy = mappings.stream()
+//                        .filter(mapping -> mapping.getPatternParser() == null)
+//                        .collect(Collectors.toList());
+//                mappings.clear();
+//                mappings.addAll(copy);
+//            }
+//
+//            @SuppressWarnings("unchecked")
+//            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+//                try {
+//                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+//                    field.setAccessible(true);
+//                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+//                } catch (IllegalArgumentException | IllegalAccessException e) {
+//                    throw new IllegalStateException(e);
+//                }
+//            }
+//        };
+//    }
+
+}
