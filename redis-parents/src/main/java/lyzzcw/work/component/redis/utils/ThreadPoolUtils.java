becdbfe103969684d77d87f216a6c6db88e720f5
lzy <1204375052@qq.com>
2023-12-22 13:51:13 +0800

    调整包结构

--- redis-parents/src/main/java/lyzzcw/work/component/redis/utils/ThreadPoolUtils.java
+++ redis-parents/src/main/java/lyzzcw/work/component/redis/utils/ThreadPoolUtils.java
@@ -1,18 +1,4 @@
-package lyzzcw.work.component.redis.utils; /**
- * Copyright 2022-9999 the original author or authors.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package lyzzcw.work.component.redis.utils;
 
 
 import java.util.concurrent.*;
@@ -37,7 +23,7 @@ public class ThreadPoolUtils {
         executor.execute(command);
     }
 
-    public static <T> Future<T> shumit(Callable<T> task){
+    public static <T> Future<T> submit(Callable<T> task){
         return executor.submit(task);
     }
 
